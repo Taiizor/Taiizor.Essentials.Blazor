dc35ecd7e9fd9890418acb6261eb1e4f6cd11824
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-11-18 01:22:11 +0300

    Infrastructure Preparation 7

---
+++ demo/Taiizor.Essentials.Maui.Conforyon/Usage/Initialize/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Taiizor.Essentials.Blazor.Conforyon.Extension;
+
+namespace Initialize
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            MauiAppBuilder builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.UseConforyon();
+
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
