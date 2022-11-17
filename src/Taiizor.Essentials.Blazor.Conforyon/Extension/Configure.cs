43e686b24f2339e75b653c3bfc14b691114af82d
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-11-18 00:51:52 +0300

    Infrastructure Preparation 6

---
+++ src/Taiizor.Essentials.Blazor.Conforyon/Extension/Configure.cs
@@ -0,0 +1,12 @@
+﻿using Taiizor.Essentials.Blazor.Conforyon.Value;
+
+namespace Taiizor.Essentials.Blazor.Conforyon.Extension
+{
+    public static class Configure
+    {
+        public static void UseConforyon(this object Builder)
+        {
+            Internal.ConforyonState = true;
+        }
+    }
+}
